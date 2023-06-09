963c7ce134aa9b1715791f8d108fc542a8e3bc8d
alexandre-mace <helloalexandremace@protonmail.com>
2023-06-09 10:03:30 +0200

    Update wording

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Matomo from "@/components/Matomo";
 export default function Home() {
   return (
     <main>
-      {/*<Matomo />*/}
+      <Matomo />
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 sm:pb-32 pt-20 text-center lg:pt-32">
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
           Calculez votre
